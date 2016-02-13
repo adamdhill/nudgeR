98a3229c1a06ded25f6ab4074bf18523187aa97f
Adam Hill <adamdhill@users.noreply.github.com>
2016-02-13 14:13:44 -0800

    Update API.R

--- API.R
+++ API.R
@@ -1,21 +1,32 @@
+#install.packages("devtools")
+library(devtools)
+ 
+#devtools::install_github("joyofdata/RTwitterAPI")
+library(RTwitterAPI)
+ 
+ 
+params <- c(
+  "oauth_consumer_key"     = "fzbcjBRjd48TJifhLAITlPf6c", 
+  "oauth_nonce"            = NA,
+  "oauth_signature_method" = "HMAC-SHA1",
+  "oauth_timestamp"        = NA,
+  "oauth_token"            = "2739148489-rVTN5u2G1ApIcAmgsjRLIZZ4ew2Lf9x921bn4kl",
+  "oauth_version"          = "1.0",
+  "consumer_secret"        = "9XdLtKKCbdRdKIjfKVaUpfsTsZ03hXVOsCkvMw9kDzDgen6iGG",
+  "oauth_token_secret"     = "TimcSexPVV2Ws6ooRXyvinDqRAzpjZbXRgTk0KxsJfH4f"
+);
+ 
+url <- "https://api.twitter.com/1.1/search/tweets.json"
+
+query <- c(q="default behavior")
+ 
+result <- RTwitterAPI::twitter_api_call(url, query, params)
+
+
+
 library(jsonlite)
 library(httr)
 
 #Set consumer_key and consumer_secret as environmental variables
 consumer_key <- "...";
 consumer_secret <- "...";
-
-
-#Auth
-secret <- RCurl::base64(paste(consumer_key, consumer_secret, sep = ":"));
-req <- POST("https://api.twitter.com/oauth2/token",
-  add_headers(
-    "Authorization" = paste("Basic", secret),
-    "Content-Type" = "application/x-www-form-urlencoded;charset=UTF-8"
-  ),
-  body = "grant_type=client_credentials"
-);
-
-#Get access token
-token <- paste("Bearer", content(req)$access_token)
-
